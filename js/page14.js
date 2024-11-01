38cf12da312fa7d3de1c037244bad5771d3fd9f0
hungt <tuanhungvu0912@gmail.com>
2024-11-01 16:36:29 +0700

    update header and cart

--- js/page14.js
+++ js/page14.js
@@ -3,29 +3,37 @@ const minusAll = document.querySelectorAll(".bx-minus");
 const plusAll = document.querySelectorAll(".bx-plus");
 const trashAll = document.querySelectorAll(".bx-trash");
 const quantities = document.querySelectorAll(".quantity-text");
+const total = document.getElementById("total");
+const grand_total = document.getElementById("grand_total");
 let number = cartItems.length;
 
-minusAll.forEach(function(minus, index) {
-    minus.addEventListener("click", function() {
+minusAll.forEach(function (minus, index) {
+    minus.addEventListener("click", function () {
         let currentQuantity = parseInt(quantities[index].textContent);
-        if(currentQuantity > 1) {
+        if (currentQuantity > 1) {
             quantities[index].textContent = currentQuantity - 1;
         }
     })
 })
-plusAll.forEach(function(plus, index) {
-    plus.addEventListener("click", function() {
+plusAll.forEach(function (plus, index) {
+    plus.addEventListener("click", function () {
         let currentQuantity = parseInt(quantities[index].textContent);
         quantities[index].textContent = currentQuantity + 1;
     })
 })
 
-trashAll.forEach(function(trash, index) {
-    trash.addEventListener("click", function() {
+trashAll.forEach(function (trash, index) {
+    trash.addEventListener("click", function () {
         cartItems[index].remove();
         number--;
-        if(number === 0) {
+        if (number === 0) {
             document.querySelector(".cart-empty").style.display = "block";
         }
     })
 })
+
+
+
+function onValueChanged() {
+    grand_total = "$" + total - 5;
+}
